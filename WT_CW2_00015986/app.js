643000a0fd23da62e7de7be68b36c28237b44796
00015986 <00015986wiut@gmail.com>
2024-02-22 17:52:15 +0500

    created main page for the CRUD program

--- WT_CW2_00015986/app.js
+++ WT_CW2_00015986/app.js
@@ -18,6 +18,7 @@ app.set("view engine", "pug");
 
 app.use("/css", express.static("public/css"));
 app.use("/javaScript", express.static("public/javaScript"));
+app.use("/images", express.static("public/images"));
 
 app.use("/api", api_route);
 app.use("/", web_route);
@@ -25,7 +26,8 @@ app.use("/", web_route);
 const PORT = 4000;
 
 app.get("/", (req, res) => {
-    res.send("hello world");
+    let mainPage = path.join(__dirname, "/views/user/mainPage.pug");
+    res.render(mainPage);
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
